2f0351883a1be2264657048f9808aa315d351c5d
Mateusz Proszowski <mateuszproszowski02@gmail.com>
2024-03-15 12:50:25 +0100

    second commit

--- php_01_kalkulator_kredytowy/app/calc_view.php
+++ php_01_kalkulator_kredytowy/app/calc_view.php
@@ -7,34 +7,6 @@
 </head>
 <body>
 
-<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
-	<label for="id_x">Liczba 1: </label>
-	<input id="id_x" type="text" name="x" value="<?php if(isset($x)) echo $x; ?>" /><br />
-	<label for="id_op">Operacja: </label>
-	<select name="op">
-		<option value="plus">+</option>
-		<option value="minus">-</option>
-		<option value="times">*</option>
-		<option value="div">/</option>
-	</select><br />
-	<label for="id_y">Liczba 2: </label>
-	<input id="id_y" type="text" name="y" value="<?php if(isset($y)) echo $y; ?>" /><br />
-	<input type="submit" value="Oblicz" />
-</form>	
-
-<?php
-//wyświeltenie listy błędów, jeśli istnieją
-if (isset($messages)) {
-	if (count ( $messages ) > 0) {
-		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
-		foreach ( $messages as $key => $msg ) {
-			echo '<li>'.$msg.'</li>';
-		}
-		echo '</ol>';
-	}
-}
-?>
-
 <?php if (isset($result)){ ?>
 <div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
 <?php echo 'Wynik: '.$result; ?>
